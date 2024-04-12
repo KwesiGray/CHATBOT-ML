f4400407495a373d4664c89e2dd2798a785453be
David Graham <ce-dggraham9721@st.umat.edu.gh>
2024-04-12 00:40:09 +0000

    ChatBOT

--- BOT.py
+++ BOT.py
@@ -1,1 +1,14 @@
-en
+import numpy as np      #For the array
+import json             #For the conversation dataset between the user and the CHATBOT       
+import tensorflow as tf #For training our model
+
+from tensorflow import keras #For training our model for our Tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling2D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from sklearn.preprocessing import LabelEncoder
+
+
+print("Imported all the necessary libraries")
